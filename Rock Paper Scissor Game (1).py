162c8dc7ad74dc2d97c460819c7191d32e440171
woadudrowha24-58813-2 <woadudrowya20032019@gmail.com>
2026-03-26 22:47:53 +0600

    Update Rock Paper Scissor Game (1).py

--- Rock Paper Scissor Game (1).py
+++ Rock Paper Scissor Game (1).py
@@ -25,7 +25,14 @@ scissor-paper=scissor win
 import random 
 item_list =["rock","paper","scissor"]
 
-user_choice =input("Enter your move = rock,paper,scissor=")
+user_score=0
+computer_score=0
+
+total_rounds=int(input("How many rounds do you want to play?"))
+
+for round_num in range(1, total_rounds +1):
+    print(f" Round{round_num}")
+user_choice =input("Enter your move = rock,paper,scissor=").lower()
 comp_choice =random.choice(item_list)
 
 print(f"user choice ={user_choice},Computer choice ={comp_choice}")
@@ -36,18 +43,36 @@ if user_choice==comp_choice:
 elif user_choice=="rock":
     if comp_choice=="paper":
         print("paper cover rock =computer win")
+        computer_score +=1
     else:
         print("rock smashes scissor = you win")
+        user_score+=1
 
 elif user_choice=="paper":
     if comp_choice=="scissor":
-        print("scissor cuts paper = computer win") 
+        print("scissor cuts paper = computer win")
+        computer_score +=1
     else:
         print("paper cover rock = you win") 
+        user_score +=1
 
 elif user_choice=="scissor":
     if comp_choice=="paper":
         print("scissor cuts paper = you win")
+        user_score +=1
     else:
         print("rock smashes scissor = computer win")
+        computer_score +=1
+else: 
+    print("Invalid input!No points given.")
+    print(f"score->you :{user_score} | computer:{computer_score}")
+
+print("Final result")
+print(f"Final score-> you:{user_score} | computer:{computer_score}")
+
+if user_score > computer_score:
+    print("Congratulations!!!! You are the winner!")
+elif computer_score > user_score:
+    print("Computer is the winner!!!")
+else print("The game is tie!")    
             
